1105a8d9bf0f283bfb94716f53d0a23274d1433e
Sai Sridhar <sridhar.akula001@gmail.com>
2023-01-16 16:22:25 +0530

    Settings

--- screens/Home/SettingsScreen.tsx
+++ screens/Home/SettingsScreen.tsx
@@ -1,15 +1,36 @@
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Switch } from "react-native";
 import ScreenView from "../../components/ScreenView";
 
 interface ISettingsScreenProps {}
 
 const SettingsScreen: React.FC<ISettingsScreenProps> = (props) => {
+	const [notifs, setNotifs] = React.useState(false);
 	return (
 		<ScreenView>
-			<Text>Settings Screen</Text>
+			<View style={styles.box}>
+				<Text style={styles.text}>Notifications</Text>
+				<Switch
+					value={notifs}
+					onValueChange={(val) => setNotifs(val)}
+					trackColor={{ true: "#ffc10780", false: "#eee" }}
+					thumbColor={notifs ? "goldenrod" : "#ccc"}
+				/>
+			</View>
 		</ScreenView>
 	);
 };
 
 export default SettingsScreen;
+
+const styles = StyleSheet.create({
+	box: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	text: {
+		fontSize: 18,
+		textTransform: "uppercase",
+	},
+});
